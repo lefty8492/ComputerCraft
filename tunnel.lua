af03426b4f6c0db187748d2b250326e1300ce077
lefty8492 <lefty8492@gmail.com>
2017-05-18 20:17:26 +0900

    APIプロジェクトからタートル用プログラムをまとめるプロジェクトに方針転換

---
+++ tunnel.lua
@@ -0,0 +1,33 @@
+os.loadAPI("rom/myApi/turtleEx")
+
+local Args = {...}
+local length = Args[1]
+
+local torch = turtleEx.searchItem("minecraft:torch")
+if not torch.found then
+	print("Torch is not found.")
+	return
+elseif torch.count < (length / 4) then
+	print("Number of torch is small.")
+	return
+elseif turtleEx.refuel(length * 2) < (length * 2) then
+	print("FuelLevel is too short.")
+	return
+end
+
+for i = 1, length do
+	while turtle.detect() do
+		turtle.dig()
+	end
+	turtle.forward()
+	turtle.digDown()
+	if (i % 4) == 0 then
+		turtle.select(torch.slot)
+		turtle.placeDown(1)
+	end
+end
+
+for i = 1, length do
+	turtle.back()
+end
+
